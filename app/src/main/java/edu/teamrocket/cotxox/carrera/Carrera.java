2ab8808cb63c2b9c846df8af997ad2acd02b90e7
juan071825 <juanmateoalvarez@gmail.com>
2026-05-01 17:55:17 +0200

    feat(Carrera.java): :sparkles: Añadir lógica a liberraConductor y asignarConductor.

--- app/src/main/java/edu/teamrocket/cotxox/carrera/Carrera.java
+++ app/src/main/java/edu/teamrocket/cotxox/carrera/Carrera.java
@@ -1,5 +1,6 @@
 package edu.teamrocket.cotxox.carrera;
 
+import edu.teamrocket.cotxox.conductores.Conductor;
 import edu.teamrocket.cotxox.conductores.PoolConductores;
 
 public class Carrera {
@@ -12,6 +13,7 @@ public class Carrera {
     private int tiempoCarrera;
     private double costeTotal;
     private int propina;
+    private Conductor conductor;
 
 
     public Carrera(String tarjetaCredito){
@@ -79,15 +81,30 @@ public class Carrera {
         return this.propina;
     }
     
+    public double getCosteEsperado(){
+        return Tarifa.getInstancia().CosteTotalEsperado(this);
+    }
 
 
-    public void asignarConductor(PoolConductores conductor){};
+    public void asignarConductor(PoolConductores conductores){
+       this.conductor = conductores.asignarConductor();
+    };
 
-    public void realizarPago(double costeTotal){};
+
+    public Conductor getConductor() {
+        return this.conductor;
+    }
+
+
+    public void realizarPago(double costeTotal){
+
+    };
 
     public void recibirPropina(int propina){};
 
-    public void liberarConductor();
+    public void liberarConductor(){
+        getConductor().setOcupado(false);
+    }
 
 
 }
