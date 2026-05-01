c393aa0bfbd498304d64aac2fd27dae152ec01ce
juan071825 <juanmateoalvarez@gmail.com>
2026-05-01 12:48:20 +0200

    feat(Tarifa.java): :sparkles: Crear clase tarifa con patrón singleton.

---
+++ app/src/main/java/edu/teamrocket/cotxox/carrera/Tarifa.java
@@ -0,0 +1,41 @@
+package edu.teamrocket.cotxox.carrera;
+
+public class Tarifa {
+
+
+    private static Tarifa instancia;
+
+    private final double COSTE_MILLA = 15;
+    private final double COSTE_MINUTO = 1.6;
+    private final double COSTE_MINIMO = 1.2;
+
+
+    private Tarifa(){};
+
+
+    public static Tarifa getInstancia(){
+
+        if (instancia == null){
+            instancia = new Tarifa();
+        }
+        return instancia;
+    }
+
+
+
+    public double getCosteDistancia(double distancia){
+        return COSTE_MILLA * distancia;
+    }
+
+
+    public double getCosteTiempo(int tiempo){
+        return COSTE_MINUTO * tiempo;
+    }
+
+
+    public double TotalEsperado(Carrera carrera){
+        return COSTE_MINIMO 
+             + getCosteDistancia(carrera.getDistancia())
+             + getCosteTiempo(carrera.getTiempoCarrera());
+    }
+}
